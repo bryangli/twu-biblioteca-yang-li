10a81dbf756f03e273072694a83983a406b664d5
Bryan Li <by623li@berkeley.edu>
2020-08-04 03:06:01 -0700

    Added in functions for return, check out and exit

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,24 +2,29 @@ package com.twu.biblioteca;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
     private static List<Book> books = new ArrayList<>();
+    private static List<Book> checkedOutBooks =  new ArrayList<>();
+    private static String[] options = {"List of Books", "Check Out", "Return", "EXIT"};
 
     private static void loadBooks() {
+        // An example of three books
         books.add(new Book("Little Prince", 1943, "Antoine de Saint-Exupery"));
         books.add(new Book("The Great Gatsby", 1925, "F. Scott Fitzgerald"));
         books.add(new Book("Pride and Prejudice", 1813, "Jane Austen"));
     }
 
     private static void displayBooks() {
-        loadBooks();
+        // Calculate the longest book name for formatting purposes
         int maxTitleLen = 0;
         for (int i = 0; i < books.size(); i++) {
             maxTitleLen = books.get(i).getTitle().length() > maxTitleLen ? books.get(i).getTitle().length() : maxTitleLen;
         }
 
-        System.out.println("\nBelow are the books available here at Biblioteca:\n");
+        // Print out a list of books
+        System.out.println("Below are the books available here at Biblioteca:\n");
         for (int i = 0; i < books.size(); i++) {
             Book currentBook = books.get(i);
             System.out.println(currentBook.getPublicationYear() + " | " +
@@ -28,8 +33,87 @@ public class BibliotecaApp {
         }
     }
 
+    private static void checkOut(String bookName) {
+        for (Book b: books) {
+            if (b.getTitle().toLowerCase().equals(bookName.toLowerCase())) {
+                checkedOutBooks.add(b);
+                books.remove(b);
+                System.out.println("Thank you! Enjoy the book :)");
+                return;
+            }
+        }
+        System.out.println("Sorry, that book is not available :(");
+    }
+
+    private static void returnBook(String bookName) {
+        for (Book b: checkedOutBooks) {
+            if (b.getTitle().toLowerCase().equals(bookName.toLowerCase())) {
+                books.add(b);
+                checkedOutBooks.remove(b);
+                System.out.println("Thank you for returning the book :)");
+                return;
+            }
+        }
+        System.out.println("That is not a valid book to return :(");
+    }
+
+    private static void takeAction(String command) {
+        // Check if the actions that customers enter exist in the action library
+        boolean availableOption = false;
+
+        if (command.toLowerCase().equals("exit")) {
+            return;
+        }
+
+        for (String s: options) {
+            if (s.toLowerCase().equals(command.toLowerCase())) {
+                availableOption = true;
+                break;
+            }
+        }
+
+        // Take corresponding actions for the messages that customers enter; if the action does not exist, re-enter
+        if (availableOption) {
+            if (command.toLowerCase().equals("list of books")) {
+                displayBooks();
+            } else if (command.toLowerCase().equals("check out")) {
+                System.out.print("\nWhich book would you like to check out? ");
+                Scanner inBook = new Scanner(System.in);
+                String bookName = inBook.nextLine();
+                checkOut(bookName);
+            } else if (command.toLowerCase().equals("return")) {
+                System.out.print("\nPlease enter the book you are returning: ");
+                Scanner inBook = new Scanner(System.in);
+                String bookName = inBook.nextLine();
+                returnBook(bookName);
+            }
+            System.out.print("\nNext: ");
+            Scanner in = new Scanner(System.in);
+            String nextCommand = in.nextLine();
+            takeAction(nextCommand);
+        } else {
+            System.out.print("\nPlease enter a valid option: ");
+            Scanner in = new Scanner(System.in);
+            String nextCommand = in.nextLine();
+            takeAction(nextCommand);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("\nWelcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-        displayBooks();
+        // Welcome message and show a list of options that a customer can take
+        loadBooks();
+        System.out.println("\nWelcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n");
+        System.out.println("Here are some options to explore:");
+        for (String s: options) {
+            System.out.println(" ".repeat(10) + s);
+        }
+
+        // An input area for the customers to select what they want to do next
+        System.out.print("\nPlease enter: ");
+        Scanner in = new Scanner(System.in);
+
+        // Read the action that customers input and take corresponding actions
+        String command = in.nextLine();
+        takeAction(command);
     }
 }
